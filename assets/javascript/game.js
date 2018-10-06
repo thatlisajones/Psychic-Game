622adc599594eea96a4dc27ed05b223b56bb7afb
That Lisa <thatlisajones@gmail.com>
2018-10-05 23:25:52 -0600

    Lots of trouble with innerHTML

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,3 @@
-
-
-// Define variables for guessing letters and scoring
-
 var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var wins = 0;
 var losses = 0;
@@ -10,18 +6,11 @@ var guessesLeft, guessedLetters, letterToGuess;
 resetGame();
 display();
 
-// Tell computer to randomly choose a letter
-
-letterToGuess = letters[Math.floor(Math.random() * letters.length)];
-  console.log("Letter to guess: " + letterToGuess);
-
-//Create if statements for keypress and logging guesses, wins, losses
-
 document.onkeyup = function(event) {
   var guess = event.key;
   if (guess === letterToGuess) {
     win();
-  } else if (guessesLeft - 1 === 0) {
+  } else if (guessesLeft - 1 == 0) {
     lost();
   } else {
     fail(guess);
@@ -31,14 +20,11 @@ document.onkeyup = function(event) {
 }
 
 function display() {
-  var winsP = document.getElementById("wins");
-  var lossesP = document.getElementById("losses");
-  var guessLeft = document.getElementById("guessLeft");
-  var letterGuessed = document.getElementById("guessed");
-  winsP.innerHTML = wins;
-  lossesP.innerHTML = loses;
-  guessLeft.innerHTML = guessesLeft;
-  letterGuessed.innerHTML = guessedLetters.join(',');
+  document.getElementById("wins").innerHTML = wins;
+  document.getElementById("losses").innerHTML = losses;
+  document.getElementById("guessLeft").innerHTML = guessesLeft;
+  document.getElementById("guessed").innerHTML = guessedLetters.join(',');
+ 
 }
 
 function win() {
@@ -57,13 +43,8 @@ function fail(letter) {
 }
 
 function resetGame() {
-  guessesLeft = 8;
+  guessesLeft = 13;
   guessedLetters = [];
   letterToGuess = letters[Math.floor(Math.random() * letters.length)];
   console.log("Letter to guess: " + letterToGuess);
 }
-
-// Create way for computer to randomly choose a letter
-
-letterToGuess = letters[Math.floor(Math.random() * letters.length)];
-  console.log("Letter to guess: " + letterToGuess);
