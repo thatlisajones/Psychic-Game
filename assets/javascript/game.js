e1dbe951b95d47cb289b09fa7371220c48100025
That Lisa <thatlisajones@gmail.com>
2018-10-06 21:20:05 -0600

    More var definitions plus textContent

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,64 +1,43 @@
-var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-
-var computerChoice = letters[Math.floor(Math.random() * letters.length)];
-
 var wins = 0;
 
 var losses = 0;
 
 var guesses = 10;
 
-var lettersGuessed = 
+var lettersGuessed = []
 
-resetGame();
-//display();
+// Computer selects letter at random.
 
+var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-  document.onkeyup = function(event) {
-  var guess = event.key;
-  if (guess === computerChoice) {
-    win();
-  } else if (guesses - 1 === 0) {
-    lose();
-  } else {
-    fail(guess);
-  }
+var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
+    
+// User guesses a letter by pressing a key. Show letters guessed. If the letter matches computer, increment wins. Else, decrement guesses. If guesses equal zero, increment losses.
 
-  display();
-}
+document.onkeypress = function(event) {
+    var userGuess = event.key;
+   lettersGuessed.push(userGuess);
 
-//function display() {
-  //document.getElementById("wins").innerHTML = wins;
-  //document.getElementById("losses").innerHTML = losses;
-  //document.getElementById("guesses").innerHTML = guesses;
-  //document.getElementById("lettersGuessed").innerHTML = lettersGuessed.join(',');
- 
-//}
+        if (userGuess === computerChoice) {
+            wins++;
+            guesses = 10;
+            lettersGuessed = [];
+            computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
+        } else {
+            guesses--;
+        }
+        if (guesses === 0) {
+            losses++;
+            guesses = 10;
+            lettersGuessed = [];
+            computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
 
-function win() {
-  wins++;
-  resetGame();
-}
+        }
+// Show wins, losses, guesses, and letters on scoreboard.
 
-function lose() {
-  losses++;
-  resetGame();
-}
+document.getElementById("wins").textContent = "Wins: " + wins;
+document.getElementById("losses").textContent = "Losses: " + losses;
+document.getElementById("guesses").textContent = "Guesses: " + guesses;
+document.getElementById("lettersGuessed").textContent = "Letters guessed: " + lettersGuessed.join(',');
 
-function fail(letter) {
-  guesses--;
-  lettersGuessed.push(letter);
-}
-
-function resetGame() {
-  guesses = 10;
-  lettersGuessed = [];
-  computerChoice = letters[Math.floor(Math.random() * letters.length)];
-  console.log("Letter to guess: " + computerChoice);
-}
-
-//document.getElementById("wins").innerHTML = wins;
-//document.getElementById("losses").innerHTML = losses;
-//document.getElementById("guesses").innerHTML = guesses;
-//document.getElementById("lettersGuessed").innerHTML = lettersGuessed;
-
+};
