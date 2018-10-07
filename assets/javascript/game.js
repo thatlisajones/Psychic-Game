3178a8636f1d1bb2cf941014fe843328236b6c7c
That Lisa <thatlisajones@gmail.com>
2018-10-06 20:53:39 -0600

    Map assets to html, still trying to fix javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,17 +1,25 @@
 var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+var computerChoice = letters[Math.floor(Math.random() * letters.length)];
+
 var wins = 0;
+
 var losses = 0;
-var guessesLeft, guessedLetters, letterToGuess;
+
+var guesses = 10;
+
+var lettersGuessed = 
 
 resetGame();
-display();
+//display();
 
-document.onkeyup = function(event) {
+
+  document.onkeyup = function(event) {
   var guess = event.key;
-  if (guess === letterToGuess) {
+  if (guess === computerChoice) {
     win();
-  } else if (guessesLeft - 1 == 0) {
-    lost();
+  } else if (guesses - 1 === 0) {
+    lose();
   } else {
     fail(guess);
   }
@@ -19,32 +27,38 @@ document.onkeyup = function(event) {
   display();
 }
 
-function display() {
-  document.getElementById("wins").innerHTML = wins;
-  document.getElementById("losses").innerHTML = losses;
-  document.getElementById("guessLeft").innerHTML = guessesLeft;
-  document.getElementById("guessed").innerHTML = guessedLetters.join(',');
+//function display() {
+  //document.getElementById("wins").innerHTML = wins;
+  //document.getElementById("losses").innerHTML = losses;
+  //document.getElementById("guesses").innerHTML = guesses;
+  //document.getElementById("lettersGuessed").innerHTML = lettersGuessed.join(',');
  
-}
+//}
 
 function win() {
   wins++;
   resetGame();
 }
 
-function lost() {
-  loses++;
+function lose() {
+  losses++;
   resetGame();
 }
 
 function fail(letter) {
-  guessesLeft--;
-  guessedLetters.push(letter);
+  guesses--;
+  lettersGuessed.push(letter);
 }
 
 function resetGame() {
-  guessesLeft = 13;
-  guessedLetters = [];
-  letterToGuess = letters[Math.floor(Math.random() * letters.length)];
-  console.log("Letter to guess: " + letterToGuess);
+  guesses = 10;
+  lettersGuessed = [];
+  computerChoice = letters[Math.floor(Math.random() * letters.length)];
+  console.log("Letter to guess: " + computerChoice);
 }
+
+//document.getElementById("wins").innerHTML = wins;
+//document.getElementById("losses").innerHTML = losses;
+//document.getElementById("guesses").innerHTML = guesses;
+//document.getElementById("lettersGuessed").innerHTML = lettersGuessed;
+
